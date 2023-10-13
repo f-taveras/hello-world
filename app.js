245a4247ba6a9c566e15ccbff7b9c16cddf98cb9
Felix A. Taveras <felixataveras1@gmail.com>
2023-10-13 03:26:24 -0400

    Finished - will add more later

--- app.js
+++ app.js
@@ -55,14 +55,15 @@ function userAnswer(){
 
 function bonus(){
 
-alert("You got it right!")
-let number = prompt("Now you get to pick a any multiplier! Pick any number")
+alert("You got it right!");
+let number = prompt("Now you get to pick a any multiplier! Pick any number");
 
 while(number > 10){
 number = prompt ("You have been greety, But I'm nice, so try again")
+}
 for (let i = 1; i <= number; i++) {
     document.write('<img class="alertImg" src="anchovy.png" alt="yes, anchovy" >');
-  }
+
 
 }
 
